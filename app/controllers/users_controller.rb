72a990ffce76b9af749990d1b1189c4702a117bf
Cindy Mit <cindymit89@gmail.com>
2021-08-16 00:14:07 -0400

    Updated application and user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :update, :destroy]
+  before_action :authorize_request, only: [:index, :show, :update, :destroy]
 
   # GET /users
   def index
